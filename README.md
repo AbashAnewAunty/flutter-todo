b91cd1405b8a982839d28c79f1c5b9e1b8453cf7
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2024-01-16 00:00:52 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_todo_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/xyz/zebracrypto/todo/my_todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.zebracrypto.todo.my_todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
